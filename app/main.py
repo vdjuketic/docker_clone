93a7af762abe2103bc9a8b16edb5add3175d12a4
vdjuketic <v.djuketic@gmail.com>
2023-11-28 23:46:11 +0100

    fourth stage

--- app/main.py
+++ app/main.py
@@ -5,13 +5,11 @@ import sys
 def main(): 
     command = sys.argv[3]
     args = sys.argv[4:]
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
     process = subprocess.Popen(
         [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     stdout, stderr = process.communicate()
-    
+
     print(stdout.decode("utf-8"), end="")
     print(stderr.decode("utf-8"), file=sys.stderr, end="")
 
