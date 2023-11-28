67731dde57beb3db8de5d766dd95a27027787233
vdjuketic <v.djuketic@gmail.com>
2023-11-28 23:43:50 +0100

    third stage

--- app/main.py
+++ app/main.py
@@ -5,9 +5,15 @@ import sys
 def main(): 
     command = sys.argv[3]
     args = sys.argv[4:]
-    print("Logs from your program will appear here!")
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"))
+    process = subprocess.Popen(
+        [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    stdout, stderr = process.communicate()
+    
+    print(stdout.decode("utf-8"), end="")
+    print(stderr.decode("utf-8"), file=sys.stderr, end="")
 
 
 if __name__ == "__main__":
