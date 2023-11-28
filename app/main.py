2e2c3bfd0219125336b9cdbaca713648d6105f42
vdjuketic <v.djuketic@gmail.com>
2023-11-28 23:39:07 +0100

    first stage

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ import sys
 
 
 def main(): 
+    command = sys.argv[3]
+    args = sys.argv[4:]
     print("Logs from your program will appear here!")
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"))
