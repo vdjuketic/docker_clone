1f635cbf7ccaa0c19520269d2fdbaec056bb5cb7
vdjuketic <v.djuketic@gmail.com>
2023-11-28 23:51:01 +0100

    fifth stage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 import subprocess
 import sys
+import os
 
 
 def main(): 
     command = sys.argv[3]
     args = sys.argv[4:]
-    process = subprocess.Popen(
-        [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    stdout, stderr = process.communicate()
 
-    print(stdout.decode("utf-8"), end="")
-    print(stderr.decode("utf-8"), file=sys.stderr, end="")
+    completed_process = subprocess.run([command, *args], capture_output=True)
+    if completed_process.stdout:
+        print(completed_process.stdout.decode("utf-8"), file=sys.stdout, end="")
+    else:
+        print(completed_process.stderr.decode("utf-8"), file=sys.stderr, end="")
+    
+    sys.exit(completed_process.returncode)
 
 
 if __name__ == "__main__":
