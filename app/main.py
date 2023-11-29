94dbd25a63d68fe66ea7cee75fdd0714a610c61a
vdjuketic <v.djuketic@gmail.com>
2023-11-29 01:26:25 +0100

    added process isolation

--- app/main.py
+++ app/main.py
@@ -1,33 +1,38 @@
 import subprocess
 import sys
 import os
-import tempfile
 import shutil
+import ctypes
 
 
-def main(): 
+CLONE_NEWPID = 0x20000000
+
+
+def main():
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    # create tmp directory as /tmp/<path from command> 
+    # create tmp directory as /tmp/<path from command>
     tmp_dir_name = "/tmp"
     command_path = os.path.dirname(command)
     tmp_directory_path = os.path.join(tmp_dir_name, command_path[1:])
 
     os.makedirs(tmp_directory_path, exist_ok=True)
-    
+
     # copy the executable to the tmp directory
     shutil.copy2(command, tmp_directory_path)
-
-    # set the root of the executable to tmp directory
     os.chroot(tmp_dir_name)
+
+    # crate new PID namespace
+    libc = ctypes.cdll.LoadLibrary(None)
+    libc.unshare(CLONE_NEWPID)
 
     completed_process = subprocess.run([command, *args], capture_output=True)
     if completed_process.stdout:
         print(completed_process.stdout.decode("utf-8"), file=sys.stdout, end="")
     else:
         print(completed_process.stderr.decode("utf-8"), file=sys.stderr, end="")
-    
+
     sys.exit(completed_process.returncode)
 
 
