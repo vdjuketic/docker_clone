50881de8927babe2dacab47db0cc5ecbe697c1e5
vdjuketic <v.djuketic@gmail.com>
2023-11-30 20:57:43 +0100

    added download image functionality

--- app/main.py
+++ app/main.py
@@ -3,31 +3,108 @@ import sys
 import os
 import shutil
 import ctypes
+from urllib import request, error
+import re
+import json
+import tarfile
+import tempfile
+from pathlib import Path
+import encodings.idna
 
 
 CLONE_NEWPID = 0x20000000
+REGISTRY_URL = "https://registry.hub.docker.com/v2"
+AUTH_URL = "https://auth.docker.io/token"
+TMP_DIR_NAME = "/tmp"
+
+
+def switch_namespace(temp_path):
+    # switch PID namespace
+    os.chroot(temp_path)
+    libc = ctypes.CDLL(None)
+    libc.unshare(CLONE_NEWPID)
+
+
+def download_layers(image, manifest, token, destination_dir):
+    manifest_version = manifest["schemaVersion"]
+
+    digests = []
+    if manifest_version == 1:
+        for layer in manifest["manifests"]:
+            digests.append(layer["digest"])
+    elif manifest_version == 2:
+        for layer in manifest["layers"]:
+            digests.append(layer["digest"])
+
+    else:
+        raise NotImplementedError("unsupported manifest version")
+
+    with open("response.tgz", "wb") as f:
+        for digest in digests:
+            req = request.Request(
+                f"https://registry.hub.docker.com/v2/library/{image}/blobs/{digest}",
+                headers={
+                    "Authorization": f"Bearer {token}",
+                    "Accept": "application/vnd.docker.distribution.manifest.v2+json",
+                },
+            )
+            res = request.urlopen(req)
+            f.write(res.read())
+
+    tarfile.open("response.tgz").extractall(destination_dir)
+
+
+def get_manifest(image, tag, token):
+    manifest_url = f"{REGISTRY_URL}/library/{image}/manifests/{tag}"
+
+    req = request.Request(
+        manifest_url,
+        headers={
+            "Authorization": f"Bearer {token}",
+            "Accept": "application/vnd.docker.distribution.manifest.v2+json",
+        },
+    )
+
+    res = request.urlopen(req)
+    res_json = json.loads(res.read().decode("utf-8"))
+    return res_json
+
+
+def download_image(destination_dir, image, tag):
+    manifest_url = f"{REGISTRY_URL}/library/{image}/manifests/{tag}"
+
+    try:
+        res = request.urlopen(manifest_url)
+    except error.HTTPError as e:
+        auth = e.headers["www-authenticate"]
+        realm = re.search(r'realm="(.+?)"', auth).group(1)
+        service = re.search(r'service="(.+?)"', auth).group(1)
+        scope = re.search(r'scope="(.+?)"', auth).group(1)
+
+    auth_url = f"{realm}?service={service}&scope={scope}"
+    req = request.Request(auth_url)
+    res = request.urlopen(req)
+
+    auth_token = json.loads(res.read().decode("utf-8"))["token"]
+
+    manifest = get_manifest(image, tag, auth_token)
+
+    download_layers(image, manifest, auth_token, destination_dir)
 
 
 def main():
-    command = sys.argv[3]
+    img = sys.argv[2]
+    directory = sys.argv[3]
     args = sys.argv[4:]
 
-    # create tmp directory as /tmp/<path from command>
-    tmp_dir_name = "/tmp"
-    command_path = os.path.dirname(command)
-    tmp_directory_path = os.path.join(tmp_dir_name, command_path[1:])
+    tmp_dir_path = tempfile.TemporaryDirectory()
 
-    os.makedirs(tmp_directory_path, exist_ok=True)
+    image, tag = img.split(":")
+    download_image(tmp_dir_path.name, image, tag)
 
-    # copy the executable to the tmp directory
-    shutil.copy2(command, tmp_directory_path)
-    os.chroot(tmp_dir_name)
+    switch_namespace(tmp_dir_path.name)
 
-    # crate new PID namespace
-    libc = ctypes.cdll.LoadLibrary(None)
-    libc.unshare(CLONE_NEWPID)
-
-    completed_process = subprocess.run([command, *args], capture_output=True)
+    completed_process = subprocess.run([directory, *args], capture_output=True)
     if completed_process.stdout:
         print(completed_process.stdout.decode("utf-8"), file=sys.stdout, end="")
     else:
